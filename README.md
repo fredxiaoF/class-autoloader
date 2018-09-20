1eb10c871676cdd32f9b4aa49a460a84df7b7039
fred <fred@freddeiMac.local>
2018-09-20 21:36:26 +0800

    completed

---
+++ README.md
@@ -0,0 +1,84 @@
+class-autoloader
+================
+
+> class-autoloader is a class autoloader, just like Rails class autoloader
+
+## Install
+>Install with [npm](https://github.com/npm/npm)
+
+```
+$ npm install class-autoloader
+```
+
+
+## Hello world
+```sh
+npm install class-autoloader
+touch index.js
+mkdir app
+mkdir app/controllers
+touch app/controllers/articles_controller.js
+```
+
+articles_controller.js
+```js
+module.exports = class ArticlesController {
+  static hello() {
+    return "Hi!";
+  }
+}
+```
+
+index.js:
+```js
+const ClassAutoloader = require('class-autoloader');
+
+const Config = {
+  autoload_paths: ['app/controllers'],
+  cache_classes: false
+}
+
+const Loader = new ClassAutoloader(Config);
+```
+
+Run it
+```sh
+$ node -r index.js
+> ArticlesController.hello();
+Hi!
+```
+
+Now modify the file articles_controller.js 
+```js
+module.exports = class ArticlesController {
+  static hello() {
+    return 'Hello Fred!';
+  }
+}
+```
+And call again
+```sh
+$ node -r index.js
+> ArticlesController.hello();
+Hi!
+> ArticlesController.hello();
+Hello Fred!
+```
+
+## Cache classes
+You can set cache_classes with true to cache classes. It will cause no class will be autoloaded
+
+## Name scope
+You can also specify a name scope e.g.
+
+```sh
+$ mkdir app/controllers/my_scope
+$ touch app/controllers/my_scope/one_class.js
+```
+```js
+MyScope.OneClass // will cause autoload one_class.js
+```
+
+
+## License
+The MIT License, 2018 [Fred Shaw](https://github.com/fredxiaoF) ([@FredShawF](https://twitter.com/FredShawF))
---
+++ class_autoloader.js
@@ -0,0 +1,172 @@
+const Path = require('path');
+const FS = require('fs');
+
+module.exports = class ClassAutoloader {
+  
+  constructor(config={}){
+    this.config = config;
+    
+    if (!Reflect.has(config, 'autoload_paths')) throw "config.autoload_paths required!"
+    
+    if (!Reflect.has(config, 'cache_classes')) this.config.cache_classes = false;
+      
+    this._set_global_class_namescope();
+    
+    this._proxy_global_variables();
+  }
+    
+  
+  // A class namescope included all classes required with autoload
+  _set_global_class_namescope() {
+    if (!global.classes) {
+      this.global_classes = global.classes = {};
+    }
+  }
+  
+  
+  _proxy_global_variables() {
+    global.__proto__ = this._proxy_autoload(global.__proto__, true);
+  }
+  
+  
+  _proxy_autoload(obj, is_global_level = false) {
+    return new Proxy(obj, {
+      get: (tar, attr) => {
+        return this.load(attr, is_global_level ? null : tar);
+      }
+    });
+  }
+  
+  
+  load(class_name, super_scope = null){    
+    let klass = this._load_cache(class_name, super_scope);
+    
+    // Avoid console.log(xxx) will call xxx[Symbol(util.inspect)]
+    if (!klass && typeof(class_name) !== 'string') return undefined;
+    
+    if (klass) {
+      // return cache if cache_classes is enabled
+      if (this.config.cache_classes == true) return klass;
+      
+      // reload if file modified
+      if (this._is_latest(klass, class_name, super_scope)) return klass;
+    }    
+    
+    let filename = this._underscore_case(class_name);
+    let [full_file_path, file_stat] = this._detect_file(filename, super_scope);
+    
+    if (!full_file_path) return undefined;
+    
+    return file_stat.isDirectory() ? 
+      this._load_directory(class_name, full_file_path, file_stat, super_scope) : 
+      this._load_file(class_name, full_file_path, file_stat, super_scope);
+  }
+  
+  
+  _load_cache(class_name, super_scope = null) {
+    return super_scope ? super_scope[class_name] : global.classes[class_name]
+  }
+  
+  
+  _load_directory(class_name, full_path, file_stat, super_scope=null){
+    // 根据class_name创建一个命名空间类
+    let klass = new Function();
+    Reflect.defineProperty(klass, 'name', {value: class_name});
+    Reflect.defineProperty(klass, 'mtimeMs', {emumerable: false, writable: true});
+    klass.path = full_path;
+    klass.mtimeMs = file_stat.mtimeMs;
+    
+    // 设置命名空间类的自动加载
+    klass = this._proxy_autoload(klass);
+    
+    this._save_cache(klass, class_name, super_scope);
+    
+    return klass;
+  }
+  
+  
+  _load_file(class_name, full_path, file_stat, super_scope=null){
+    let klass = require(full_path);
+    Reflect.defineProperty(klass, 'mtimeMs', {emumerable: false, writable: true});
+    klass.mtimeMs = file_stat.mtimeMs;
+    
+    this._save_cache(klass, class_name, super_scope);
+    
+    return klass;
+  }
+  
+  
+  _save_cache(klass, class_name, super_scope) {
+    if (super_scope) {
+      super_scope[class_name] = klass;
+    } else {
+      global.classes[class_name] = klass;
+    }
+  }
+  
+  
+  _remove_cache(full_file_path, class_name, super_scope) {
+    if (super_scope) {
+      delete super_scope[class_name];
+    } else {
+      delete global.classes[class_name];
+    }
+    delete require.cache[require.resolve(full_file_path)];
+  }
+  
+  
+  _is_latest(klass, class_name, super_scope) {
+    let filename = this._underscore_case(class_name);
+    let [full_file_path, file_stat] = this._detect_file(filename, super_scope);
+    
+    if (klass.mtimeMs <= file_stat.mtimeMs) {
+      this._remove_cache(full_file_path, class_name, super_scope);
+      return false
+    } else {
+      return true;
+    }
+  }
+  
+  
+  _detect_file(filename, super_scope = null) {
+    if (super_scope) {
+      let full_path = Path.join(super_scope.path, filename);    
+      return this._parse_file(full_path);
+    }
+    
+    for (let root of this.config.autoload_paths){
+      let full_path = Path.join(Path.resolve(root), filename);
+      let result = this._parse_file(full_path);
+      if (result[0]) return result;
+    }
+    return []
+  }
+  
+  
+  _parse_file(full_path) {
+    try {
+      let stat = FS.lstatSync(full_path + '.js');
+      return [full_path, stat];
+    } catch (e) {
+      try {
+        let stat = FS.lstatSync(full_path);
+        return [full_path, stat];
+      } catch (e) {
+        return [];
+      }
+    }
+  }
+  
+  
+  _underscore_case(class_name) {
+    let file_name = class_name.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();});
+    file_name = file_name.replace(/^_/, '');
+    return file_name;
+  }
+  
+  
+  
+  
+  
+  
+}
